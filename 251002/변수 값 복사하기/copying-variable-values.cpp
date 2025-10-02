224d6156d9e8e2d67065c141c65d9c0716aeec28
Lifesheep1 <165856601+Lifesheep1@users.noreply.github.com>
2025-10-02 16:56:15 +0900

    Title: 변수 값 복사하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251002/변수 값 복사하기/copying-variable-values.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a =1, b=2, c=3;
+    c = a;
+    c=b;
+    cout<<a<<" "<<b<<" "<<c;
+    return 0;
+}
