631a6bbd5ec18e134459afca2a84e26e11f98128
Lifesheep1 <165856601+Lifesheep1@users.noreply.github.com>
2025-10-02 17:26:39 +0900

    Title: 길이 단위 환산하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251002/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double ft = 30.48;
+    double n;
+    cin>>n;
+    cout<<fixed;
+    cout.precision(1);
+    cout<<n*ft;
+    return 0;
+}
