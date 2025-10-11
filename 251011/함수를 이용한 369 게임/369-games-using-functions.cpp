9d29801d057e77e128c37dcec6d01421b33407ed
Lifesheep1 <165856601+Lifesheep1@users.noreply.github.com>
2025-10-11 20:25:45 +0900

    Title: 함수를 이용한 369 게임, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 251011/함수를 이용한 369 게임/369-games-using-functions.cpp
+++ 251011/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -2,28 +2,24 @@
 
 using namespace std;
 
-int a, b;
-int x,y;
-int cnt = 0;
+bool has369(int x){
+    while(x>0){
+        int d = x % 10;
+        if(d==3|| d==6 || d==9) return true;
+        x/=10;
+    }
+    return false;
+}
 void f(int n,int m){
-    for(int i=n;i<=m;i++){
-        for(int k=1;k<4;k++){
-            if(i%3==0||i%10==3*k||i/10==3*k||i/100==3*k||i/1000==3*k||i/10000==3*k||i/100000==3*k||i/1000000==3*k){
-                cnt+=1;
-                break;
-            }
-        }
+    int cnt = 0;
+    for(int i =n; i<=m; i++){
+        if(i%3 == 0 || has369(i)) cnt++;
     }
     cout<<cnt;
 }
-int main() {
-    cin >> a >> b;
-
-    
-    // Please write your code here.
+int main(){
+    int a,b;
+    cin>>a>>b;
     f(a,b);
-
-
-
     return 0;
 }
