f6b785abb421127b59977e7c2134cdd4204fe2b0
Lifesheep1 <165856601+Lifesheep1@users.noreply.github.com>
2025-10-15 16:08:43 +0900

    Title: 2021년 날짜의 유무, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251015/2021년 날짜의 유무/with-or-without-2021.cpp
+++ 251015/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -9,12 +9,14 @@ int main() {
 
     //4,6,9,11
     // Please write your code here.
-    if(M>12 || D>=32) cout<<"No"; // 터무니없는 조건 제외
+    if(M>12 || D>=32) cout<<"No"; return 0; // 터무니없는 조건 제외
     if(M==2){
         if(D>=29) cout<<"No";
+        return 0;
     }
     else if(M == 4|| M==6 || M==9 || M == 11){
         if(D>=31) cout<<"No";
+        return 0;
     }
     
     else cout<<"Yes";
