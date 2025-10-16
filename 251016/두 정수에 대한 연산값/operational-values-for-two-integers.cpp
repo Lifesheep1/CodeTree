8107e4790ddf0ccc5c82b43ffefebe97f6c92403
Lifesheep1 <165856601+Lifesheep1@users.noreply.github.com>
2025-10-16 10:22:54 +0900

    Title: 두 정수에 대한 연산값, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251016/두 정수에 대한 연산값/operational-values-for-two-integers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b;
+
+void A(int &a, int &b){
+    if(a>b){
+        a+=25;
+        b*=2;
+        cout<<b<<" "<<a;
+    }
+    else if(b>a){
+        b+=25;
+        a*=2;
+        cout<<a<<" "<<b;
+    }
+}
+int main() {
+    cin >> a >> b;
+
+    // Please write your code here.
+    A(a,b);
+
+    return 0;
+}
