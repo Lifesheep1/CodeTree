99963e7103c506b1f6569370f0077d93975f35c6
Lifesheep1 <165856601+Lifesheep1@users.noreply.github.com>
2025-10-16 11:16:25 +0900

    Title: 2개 이상의 알파벳, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251016/2개 이상의 알파벳/more-than-one-alphabet.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string A;
+int cnt = 0;
+
+int main() {
+    cin >> A;
+
+    // Please write your code here.
+    for(int i=0; i<A.length()-1; i++){
+        if(A.at(i) != A.at(i+1)){
+            cnt++;
+        }
+    }
+    if(cnt>=2) cout<<"Yes";
+    else cout<<"No";
+
+    return 0;
+}
